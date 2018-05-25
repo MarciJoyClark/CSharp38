5f78c0f84819bc606f4eb8994dcc0b1ee8292daa
MarciJoyClark <33448580+MarciJoyClark@users.noreply.github.com>
2018-05-24 18:44:01 -0700

    update3
    update3

---
+++ MathOptional/MathOptional/Option.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOptional
+{
+    public class Option
+    {
+        public static int option(int number)
+        {
+            int newOp = number * 2;
+            return newOp;
+        }
+        public static int option2(int optionalInt, int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                while (optionalInt > 0)
+                {
+                    int newOp2 = optionalInt * 10;
+                    return newOp2;
+                }
+            }
+        }              
+    }
+}
